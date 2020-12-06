490a5ab52b578e55cf193e7fbd36b28b83befd66
Recursing <buonanno.lorenzo@gmail.com>
2020-12-06 14:42:41 +0100

    no imports

--- day5.py
+++ day5.py
@@ -1,8 +1,5 @@
-from operator import and_
-from functools import reduce
-
 groups = open("day5.txt").read().split("\n\n")
 print(sum(len(set(group) - {"\n", " "}) for group in groups))
 print(
-    sum(len(reduce(and_, map(set, group.splitlines()), set(group))) for group in groups)
+    sum(len(set.intersection(*map(set, group.splitlines()))) for group in groups)
 )
